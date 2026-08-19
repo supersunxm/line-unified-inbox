e88a1f51241d1e02ee86f133cc4a17d15b7c40f5
supersunxm <chuti.mm1999@gmail.com>
2026-08-19 15:23:55 +0700

    feat(crm): link mass messages to purchase drafts

--- frontend/src/app/mass-messages/page.tsx
+++ frontend/src/app/mass-messages/page.tsx
@@ -1,5 +1,11 @@
 import { ApplicationWorkspace } from "../page";
+import { PurchaseBroadcastDraftBanner } from "./purchase-broadcast-draft-banner";
 
 export default function MassMessagesPage() {
-  return <ApplicationWorkspace initialSection="mass-messages" />;
+  return (
+    <>
+      <ApplicationWorkspace initialSection="mass-messages" />
+      <PurchaseBroadcastDraftBanner />
+    </>
+  );
 }
