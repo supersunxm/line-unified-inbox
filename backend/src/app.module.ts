a17d92fb3f30ec0182de5bf7903888cad7f07a0e
supersunxm <chuti.mm1999@gmail.com>
2026-08-19 15:55:21 +0700

    feat(crm): register safe purchase broadcast sender

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -51,12 +51,13 @@ import { PriorityService } from "./priority/priority.service";
 import { PurchaseAnalyticsController } from "./purchase-analytics.controller";
 import { PurchaseAnalyticsService } from "./purchase-analytics.service";
 import { PurchaseBroadcastAudienceService } from "./purchase-broadcast-audience.service";
+import { PurchaseBroadcastSafeSendService } from "./purchase-broadcast-safe-send.service";
 
 import { AppVersionModule } from "./app-version/app-version.module";
 
 @Module({
   imports: [PrismaModule, AuthModule, RealtimeModule, CredentialsModule, MediaModule, ClassificationModule, LineProfileModule, LineWebhookModule, LineOfficialAccountsModule, StoreMasterModule, FollowerInsightsModule, FriendSourceLinksModule, TranslationModule, OperationsModule, ExecutiveBriefModule, BiAssistantModule, ActionAgentModule, ImpactEngineModule, AiTelemetryModule, OperationalMemoryModule, MassMessageModule, CouponModule, TikTokModule, AppVersionModule],
   controllers: [HealthController, StoresController, ConversationsController, ActivityController, DashboardController, MetadataController, OperationsController, CustomersController, MobileConversationsController, MobileNotificationsController, MobileConfigController, MobileProductsController, MonthlySummaryController, PurchaseAnalyticsController],
-  providers: [ConversationsService, LineMessagingService, DashboardAnalyticsService, OperationReportService, CustomerIntelligenceService, RootCauseService, RecommendationService, MobileConversationsService, MobileNotificationsService, MobileConfigService, MonthlySummaryService, PriorityService, PurchaseAnalyticsService, PurchaseBroadcastAudienceService],
+  providers: [ConversationsService, LineMessagingService, DashboardAnalyticsService, OperationReportService, CustomerIntelligenceService, RootCauseService, RecommendationService, MobileConversationsService, MobileNotificationsService, MobileConfigService, MonthlySummaryService, PriorityService, PurchaseAnalyticsService, PurchaseBroadcastAudienceService, PurchaseBroadcastSafeSendService],
 })
 export class AppModule {}
