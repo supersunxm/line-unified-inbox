8a93231c3391be221842986f1daed33fdfadf054
supersunxm <chuti.mm1999@gmail.com>
2026-08-28 10:14:52 +0700

    feat: register LINE stateless token manager

--- backend/src/credentials/credentials.module.ts
+++ backend/src/credentials/credentials.module.ts
@@ -1,6 +1,12 @@
 import { Global, Module } from "@nestjs/common";
+import { PrismaModule } from "../prisma.module";
 import { CredentialEncryptionService } from "./credential-encryption.service";
+import { LineStatelessTokenService } from "./line-stateless-token.service";
 
 @Global()
-@Module({ providers: [CredentialEncryptionService], exports: [CredentialEncryptionService] })
+@Module({
+  imports: [PrismaModule],
+  providers: [CredentialEncryptionService, LineStatelessTokenService],
+  exports: [CredentialEncryptionService, LineStatelessTokenService],
+})
 export class CredentialsModule {}
