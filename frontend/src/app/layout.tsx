c60456ab5b62a28fef90cb8711c7f4f7c85c0da4
supersunxm <chuti.mm1999@gmail.com>
2026-09-08 18:05:26 +0700

    chore(branding): replace public placeholder mark with OPPO logo
    * chore(branding): add OPPO wordmark asset
    
    * chore(branding): style public headers with OPPO wordmark
    
    * chore(branding): load public OPPO logo styles
    
    * docs(branding): note public logo scope
    
    * chore(branding): remove temporary note
    
    * chore(branding): mark logo-only branch
    
    * chore(branding): remove temporary branch marker

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./public-branding.css";
 import { ThemeProvider } from "./theme";
 import { LanguageProvider } from "./language";
 
